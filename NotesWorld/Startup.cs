1a3c6d1062bb44cd0b74e2e938bba9e0f338efa3
Abhishek kashyap <kashyap1401024@outlook.com>
2018-10-29 20:37:04 +0530

    This is the first change

---
+++ NotesWorld/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NotesWorld.Startup))]
+namespace NotesWorld
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
